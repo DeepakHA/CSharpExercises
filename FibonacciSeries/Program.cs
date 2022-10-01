284bfa865693f9213c8b9d2f51ac6472146fd9e2
Deepak H A <deepakha1993@gmail.com>
2022-10-01 22:14:04 +0530

    Fibonacci Sequence.

---
+++ FibonacciSeries/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Program to print the Fibanacci sequence of numbers below the inputted number");
+Console.WriteLine("Enter a positive number:");
+int number = int.Parse(Console.ReadLine());
+
+int firstNum = 1, secondNum = 1;
+int sum = 0;
+Console.WriteLine($"Fibonacci sequence below {number}:");
+if(number <= 1)
+{
+    Console.WriteLine("Enter a number greater than 1");
+    return;
+}
+
+Console.WriteLine(firstNum);
+Console.WriteLine(secondNum);
+while (sum < number)
+{
+    sum = firstNum + secondNum;
+    if (sum >= number)
+    {
+        break;
+    }
+    firstNum = secondNum;
+    secondNum = sum;
+    Console.WriteLine(sum);
+}
+
+//Below code is to print n number of fibonacci numbers
+//for (int i = 0; i < number; i++)
+//{
+//    sum = firstNum + secondNum;
+//    firstNum = secondNum;
+//    secondNum = sum;
+//    Console.WriteLine(sum);
+//}
