55ca4783469f13b9237d31e7be6c380ae95f61e7
Deepak H A <deepakha1993@gmail.com>
2022-11-10 20:32:20 +0530

    Project - Multiples of a number.

---
+++ MultiplesOfNumber/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Enter a number:");
+int num = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter a length of multiple:");
+int len = int.Parse(Console.ReadLine());
+
+Console.WriteLine();
+
+for (int i = 1; i <= len; i++)
+{
+    Console.WriteLine(num*i);
+}
+
